b5902f3f2794784b332bbdae34b6b0356e34040e
martincz <martincz@mokeedev.com>
2017-03-15 19:59:08 +0800

    Drep
    Change-Id: Ibca027ad11c6a10e60d041bcdc4313de1f427052

--- Android.mk
+++ Android.mk
@@ -1,14 +1,22 @@
-# Copyright 2009 The Android Open Source Project
+# Copyright 2017 The MoKee Open Source Project
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := builtins.c \
-	edify_checker.c
-LOCAL_STATIC_LIBRARIES += libedify
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../bootable/recovery
-LOCAL_MODULE := edify_checker
 
+LOCAL_IS_HOST_MODULE := true
+LOCAL_MODULE := libedify-host
+LOCAL_ADDITIONAL_DEPENDENCIES := bootable/recovery/edify/Android.mk
+LOCAL_C_INCLUDES += bootable/recovery
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_CLANG := true
+LOCAL_SRC_FILES := builtins.cpp edify_checker.cpp
+LOCAL_MODULE := edify_checker
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_STATIC_LIBRARIES += libedify-host
 
 include $(BUILD_HOST_EXECUTABLE)
